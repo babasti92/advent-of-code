ef8130b378c6c52a5edd3ae95a05a7de0f2e1984
Barbara Stierli-Lopez <barbara.stierli-lopez@zkb.ch>
2024-12-02 13:26:37 +0100

    test commit from eap

---
+++ src/main/java/aoc/year2024/Day02.java
@@ -0,0 +1,18 @@
+package aoc.year2024;
+
+import java.util.List;
+
+import aoc.Day;
+
+public class Day02 implements Day {
+
+  @Override
+  public String part1(List<String> input) {
+    return null;
+  }
+
+  @Override
+  public String part2(List<String> input) {
+    return null;
+  }
+}
